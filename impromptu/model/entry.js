373dd3c7279cfb40bffe54eec4f9e848572eccb9
Elli Raynai <elli@elliraynai.com>
2021-06-20 15:28:55 -0400

    set up seeds file to test out referencing

--- impromptu/model/entry.js
+++ impromptu/model/entry.js
@@ -2,12 +2,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const entrySchema = new Schema({
-    date: { type: Date, required: true },
+    date: { type: Date },
     title: { type: String, required: true },
-    mood: { type: String, required: true },
+    mood: { type: String, lowercase: true, enum: ['glad', 'sad', 'mad'] },
     entry: { type: String, required: true },
 }, {
     timestamps: true,
 });
 
-module.exports = mongoose.model('Entry', entrySchema);
+
+const Entry = mongoose.model('Entry', entrySchema);
+module.exports = Entry;
---
+++ impromptu/seeds.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const Entry = require('./model/entry');
+const EntryTitle = require('./model/entryTitle');
+
+mongoose.connect('mongodb://localhost/diaryEntries', {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useCreateIndex: true,
+});
+
+const db = mongoose.connection;
+
+db.on('connected', function () {
+	console.log(`Connected to MongoDB at ${db.host}:${db.port}`);
+});
+
+const d = new Entry({
+    title: 'This is my first blog post',
+    mood: 'sad',
+    entry: 'This is just the beginning of something much bigger than myself',
+});
+
+const ent = new EntryTitle({
+    title: d.title,
+    mood: d.mood,
+});
+
+d.save().then(d => {
+    console.log(d)
+})
+.catch(e => {
+    console.log(e);
+})
+
+ent.save().then(ent => {
+    console.log(ent)
+})
+.catch(e => {
+    console.log(e)
+})
